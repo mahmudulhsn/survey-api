e9f77a4a65cff69de1bb40d5f33b3c08edb8f6f6
M H Hasib <hasib.mahmudul@gmail.com>
2023-06-13 15:15:27 +0600

    feat: authentication done

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,11 +4,33 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\App;
+use Illuminate\Database\QueryException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
+     * Holds env
+     *
+     * @var
+     */
+    const PRODUCTION_ENV = 'production';
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array<int, string>
      */
@@ -20,11 +42,62 @@ class Handler extends ExceptionHandler
 
     /**
      * Register the exception handling callbacks for the application.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $exception): JsonResponse
+    {
+        if ($exception instanceof ValidationException) {
+            $statusCode = 422;
+            $message = $exception->getMessage();
+            $errors = $exception->errors();
+        } elseif ($exception instanceof HttpException) {
+            $statusCode = $exception->getStatusCode();
+            $message =
+                $statusCode == 404 ? "Not Found!" : $exception->getMessage();
+        } elseif ($exception instanceof AuthenticationException) {
+            $statusCode = 401;
+            $message =
+                App::environment("production") && $exception->getCode() == 500
+                ? "Something Went Wrong!"
+                : $exception->getMessage();
+        } else {
+            if ($exception instanceof QueryException) {
+                DB::rollback();
+            }
+
+            $statusCode =
+                $exception->getCode() >= 400 && $exception->getCode() <= 499
+                ? $exception->getCode()
+                : 500;
+            $message =
+                App::environment("production") && $exception->getCode() == 500
+                ? "Something Went Wrong!"
+                : $exception->getMessage();
+        }
+
+        return response()->json(
+            [
+                "success" => false,
+                "status_code" => $statusCode,
+                "message" => $message,
+                "errors" => isset($errors) ? $errors : [],
+            ],
+            $statusCode,
+        );
     }
 }
---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SignupRequest;
+use App\Http\Requests\UserRegisterRequest;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Signup
+     * @param \App\Http\Requests\SignupRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function signup(UserRegisterRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    /**
+     * Summary of login
+     * @param \App\Http\Requests\LoginRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(LoginRequest $request): Response
+    {
+        $credentials = $request->validated();
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+        if (!Auth::attempt($credentials, $remember)) {
+            return response([
+                'error' => 'The Provided credentials are not correct'
+            ], 422);
+        }
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    /**
+     * Summary of logout
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(): Response
+    {
+        /** @var User $user */
+        $user = Auth::user();
+        // Revoke the token that was used to authenticate the current request...
+        $user->currentAccessToken()->delete();
+
+        return response([
+            'success' => true
+        ]);
+    }
+
+    /**
+     * Summary of me
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function me(Request $request): Response
+    {
+        return response([
+            'user' => $request->user()
+        ]);
+    }
+}
